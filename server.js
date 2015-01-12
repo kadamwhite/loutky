2a2fa4b0ee507acedfa4b178c4ca01b91ed8d443
K.Adam White <kadamwhite@gmail.com>
2015-01-12 18:15:15 -0500

    Add basic server, with skeleton CSS

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log('not implemented');
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var browserify = require('browserify-middleware');
+
+var app = express();
+
+
+// Middleware setup
+app.use( favicon( __dirname + '/public/images/favicon.png' ) );
+
+// Serve static assets
+app.use( express.static( path.join( __dirname, 'public' ) ) );
+
+// Single, catch-all router
+app.use( /\/\w*/, function( req, res, next ) {
+  res.sendFile( __dirname + '/public/index.html' );
+});
+
+// Initialize the server
+app.set( 'port', process.env.PORT || 3000 );
+
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
