fad6ad0064751d0aba38477f16611d87a6e65abb
K. Adam White <kadamwhite@users.noreply.github.com>
2015-06-12 21:56:40 -0500

    Prompt before installing packages

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ function parse_git_hash() {
   git rev-parse HEAD 2> /dev/null | sed "s/\(.*\)/\1/"
 }
 
+echo
 read -p "What should the folder name be for this project? " DIRNAME
 echo "Creating directory '$DIRNAME'..."
 mv loutky $DIRNAME
@@ -19,7 +20,7 @@ rm -rf .git
 git init
 
 echo "Ignoring Sublime project files..."
-echo "" >> .git/info/exclude
+echo >> .git/info/exclude
 echo "*.sublime-project" >> .git/info/exclude
 echo "*.sublime-workspace" >> .git/info/exclude
 
@@ -30,8 +31,14 @@ echo "Creating first commit..."
 git add .
 git commit -am "Loutky boilerplate @ ${GIT_HASH}"
 
-echo "Installing package dependencies..."
-npm install
+echo
+read -p "Install package dependencies now? [y/n] " -n 1 -r INSTALL_NOW
+echo
+if [[ $INSTALL_NOW =~ ^[Yy]$ ]]
+then
+  echo "Installing package dependencies..."
+  npm install
+fi
 
-echo ""
+echo
 echo "Loutky project installed to ./$DIRNAME!"
