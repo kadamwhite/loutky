f2e82c4dc3ca143e8b4e5be00cdb71b533b47970
K. Adam White <kadamwhite@users.noreply.github.com>
2015-06-12 21:50:45 -0500

    Fix issues with setup script

--- init.sh
+++ init.sh
@@ -2,16 +2,16 @@
 
 # Useful helper function, adapted from of https://gist.github.com/dciccale/5560837
 function parse_git_hash() {
-  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/\1/"
+  git rev-parse HEAD 2> /dev/null | sed "s/\(.*\)/\1/"
 }
 
-read -p "What should the folder name be for this project?" $dirname
-echo "Creating directory '$dirname'..."
-mv loutky $dirname
-cd $dirname
+read -p "What should the folder name be for this project? " DIRNAME
+echo "Creating directory '$DIRNAME'..."
+mv loutky $DIRNAME
+cd $DIRNAME
 
 echo "Creating Sublime project files..."
-mv loutky.sublime-project $dirname.sublime-project
+mv loutky.sublime-project $DIRNAME.sublime-project
 
 echo "Creating Git repository..."
 GIT_HASH=$(parse_git_hash)
@@ -25,7 +25,7 @@ echo "*.sublime-workspace" >> .git/info/exclude
 
 echo "Creating first commit..."
 git add .
-git commit -m 'Loutky boilerplate @ ${GIT_HASH}'
+git commit -am "Loutky boilerplate @ ${GIT_HASH}"
 
 echo "Removing setup script..."
 rm init.sh
@@ -34,4 +34,4 @@ echo "Installing package dependencies..."
 npm install
 
 echo ""
-echo "Loutky project installed!"
+echo "Loutky project installed to ./$DIRNAME!"
