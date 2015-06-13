028918565f64a7f19f27d0fdefca7634df994fa0
K. Adam White <kadamwhite@users.noreply.github.com>
2015-06-12 19:48:31 -0500

    Add section comments to app.js

--- app.js
+++ app.js
@@ -10,17 +10,25 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// View engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// Logging & Parsing
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Middleware setup
+
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Routing
 
 app.use('/', routes);
 app.use('/users', users);
